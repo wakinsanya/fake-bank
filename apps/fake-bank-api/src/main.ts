380a6ce749bd4a5d2d61ef1c0ad5d65769304e44
William Akinsanya <akinsanya.will@gmail.com>
2019-11-10 18:23:15 +0000

    Added helmet and compression to app.

--- apps/fake-bank-api/src/main.ts
+++ apps/fake-bank-api/src/main.ts
@@ -1,11 +1,11 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- **/
-
 import * as express from 'express';
+import * as helmet from 'helmet';
+import * as compression from 'compression';
 
 const app = express();
+
+app.use(helmet);
+app.use(compression());
 
 app.get('/api', (req, res) => {
   res.send({ message: 'Welcome to fake-bank-api!' });
@@ -15,4 +15,5 @@ const port = process.env.port || 3333;
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
 });
+
 server.on('error', console.error);
