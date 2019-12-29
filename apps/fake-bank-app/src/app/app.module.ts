24eba452a481b9456a2f97a021e259cb34419175
William Akinsanya <akinsanya.will@gmail.com>
2019-12-29 23:32:31 +0000

    add nebular

--- apps/fake-bank-app/src/app/app.module.ts
+++ apps/fake-bank-app/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from '@angular/material/icon';
 import { HttpClientModule } from '@angular/common/http';
 import { CoreModule } from '@fake-bank/core/core.module';
+import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,7 +18,10 @@ import { CoreModule } from '@fake-bank/core/core.module';
     BrowserAnimationsModule,
     MatIconModule,
     HttpClientModule,
-    CoreModule
+    CoreModule,
+    NbThemeModule.forRoot({ name: 'default' }),
+    NbLayoutModule,
+    NbEvaIconsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
